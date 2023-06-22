26cbe5b66b0fb0361613f4eeb0cc8f7c82a29c4e
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-06-22 14:54:12 +0100

    Add stakeFromHome grayscale filter

--- src/components/DataTable/TableCells/StakeFromHome.tsx
+++ src/components/DataTable/TableCells/StakeFromHome.tsx
@@ -3,28 +3,26 @@ import { Flex, Box, Text, Image, Popover, PopoverTrigger, PopoverContent } from 
 
 export default function StakeFromHome({ provider }) {
     return (
-        <>
-            {provider.stakeFromHome && (
-                <Flex direction="column" gap={"2px"} alignItems={"center"}>
-                    <Popover placement="top" trigger="hover" openDelay={200} closeDelay={0} gutter={5}>
-                        <PopoverTrigger>
-                            <Image
-                                objectFit="contain"
-                                boxSize={"50px"}
-                                src={"./images/StakeFromHome.png"}
-                                alt={provider.logo.alt}
-                                borderRadius={"100%"}
-                            />
-                        </PopoverTrigger>
-                        <PopoverContent>
-                            <Flex direction={"column"} gap={5}>
-                                <Text>Stake from Home. Defend Ethereum.</Text>
-                                <Text>🏗️ More details coming soon! 🏗️</Text>
-                            </Flex>
-                        </PopoverContent>
-                    </Popover>
-                </Flex>
-            )}
-        </>
+        <Flex direction="column" gap={"2px"} alignItems={"center"}>
+            <Popover placement="top" trigger="hover" openDelay={200} closeDelay={0} gutter={5}>
+                <PopoverTrigger>
+                    <Image
+                        src={"./images/StakeFromHome.png"}
+                        alt={provider.logo.alt}
+                        objectFit="contain"
+                        boxSize={"50px"}
+                        borderRadius={"100%"}
+                        filter={provider.stakeFromHome ? "" : "grayscale(100%)"}
+                        opacity={provider.stakeFromHome ? "" : "0.2"}
+                    />
+                </PopoverTrigger>
+                <PopoverContent>
+                    <Flex direction={"column"} gap={5}>
+                        <Text>Stake from Home. Defend Ethereum.</Text>
+                        <Text>🏗️ More details coming soon! 🏗️</Text>
+                    </Flex>
+                </PopoverContent>
+            </Popover>
+        </Flex>
     )
 }
