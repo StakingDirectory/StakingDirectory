6d15c72c0541bf1902405750f04f9c5d884a0a4e
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-06-26 23:28:56 +0100

    Add dev phase hammer edit button

--- src/components/DataTable/DataRowMenuButton.tsx
+++ src/components/DataTable/DataRowMenuButton.tsx
@@ -13,12 +13,14 @@ import {
     ModalBody,
     ModalCloseButton,
     Button,
+    Tooltip,
+    Box,
 } from "@chakra-ui/react"
 
 import EditorModal from "./EditorModal/EditorModal"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEllipsisV, faEdit } from "@fortawesome/free-solid-svg-icons"
+import { faEllipsisV, faEdit, faHammer } from "@fortawesome/free-solid-svg-icons"
 
 export default function DataRowMenuButton({ provider }) {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -26,11 +28,32 @@ export default function DataRowMenuButton({ provider }) {
     return (
         <>
             <Menu variant={"DataTableOption"} placement="right" gutter={3}>
-                <MenuButton aria-label="Options" borderRadius={10} w={8} h={8} cursor={"pointer"} onClick={(e) => e.stopPropagation()}>
+                {/* <MenuButton aria-label="Options" borderRadius={10} w={8} h={8} cursor={"pointer"} onClick={(e) => e.stopPropagation()}>
                     <Flex justifyContent={"center"} alignItems={"center"} height={"100%"}>
                         <FontAwesomeIcon icon={faEllipsisV} />
                     </Flex>
-                </MenuButton>
+                </MenuButton> */}
+
+                {/* TODO: This whole tooltip Component should be removed after the initial launch and feedback phase */}
+                <Tooltip
+                    placement={"top"}
+                    gutter={10}
+                    label={
+                        <Flex className={"tooltipLabel"} direction={"column"} gap={2}>
+                            <Box>Does something need fixing?</Box>
+                            <Box>Let us know!</Box>
+                        </Flex>
+                    }
+                    className="tooltipArrow"
+                    hasArrow={true}
+                >
+                    <MenuButton aria-label="Options" borderRadius={10} w={8} h={8} cursor={"pointer"} onClick={(e) => e.stopPropagation()}>
+                        <Flex justifyContent={"center"} alignItems={"center"} height={"100%"}>
+                            <FontAwesomeIcon icon={faHammer} />
+                        </Flex>
+                    </MenuButton>
+                </Tooltip>
+
                 <MenuList minW={1}>
                     <MenuItem
                         onClick={(e) => {
