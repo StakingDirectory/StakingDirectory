0ed8f7fa2f2e376d529fe121ebdd8f6491dd5e6b
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-06-27 00:50:41 +0100

    Made to Built

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <Flex w={"100vw"} direction={"column"} alignItems={"center"} pb={5} px={3} gap={1}>
             <Text fontWeight={"bold"}>
-                Made with ❤️ by{" "}
+                Built with ❤️ by{" "}
                 <Link as={NextLink} href={"https://twitter.com/EridianAlpha"} color={"blue"} textDecoration={"underline"} target="_blank">
                     Eridian
                 </Link>
