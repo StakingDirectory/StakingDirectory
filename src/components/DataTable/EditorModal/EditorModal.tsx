50812f5b042d87f7b296ab2a78d8c3639ce3e5e8
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2023-07-02 17:08:44 +0100

    Add dynamic field rendering in editor

--- src/components/DataTable/EditorModal/EditorModal.tsx
+++ src/components/DataTable/EditorModal/EditorModal.tsx
@@ -40,6 +40,69 @@ import { faChevronDown, faRotateLeft } from "@fortawesome/free-solid-svg-icons"
 import dataProps from "public/data/dataProps"
 const providerProperties = dataProps.find((prop) => prop.id === "providerProperties").providerProperties
 
+const allOptions = [
+    {
+        id: "name",
+        headerName: "Update Name",
+        fields: [
+            {
+                id: "name",
+                name: "Name",
+                inputType: "input",
+            },
+        ],
+    },
+    {
+        id: "stakingType",
+        headerName: "Update Staking Type",
+        fields: [
+            {
+                id: "stakingType",
+                name: "Staking Type",
+                inputType: "select",
+                options: dataProps
+                    .flatMap((dataProp) => dataProp)
+                    .find((prop) => prop?.id === "stakingType")
+                    ?.options.map((option) => ({
+                        value: option.value,
+                        name: providerProperties.find((prop) => prop.value === option.value)?.name,
+                    })),
+            },
+        ],
+    },
+    {
+        id: "status",
+        headerName: "Update Status",
+        fields: [
+            {
+                id: "status",
+                name: "Status",
+                inputType: "select",
+                options: [
+                    { value: "active", name: "Active" },
+                    { value: "dev", name: "In development" },
+                ],
+            },
+        ],
+    },
+    {
+        id: "mainnetLaunch",
+        headerName: "Update Mainnet Launch Date",
+        fields: [
+            {
+                id: "mainnetLaunch.date",
+                name: "Mainnet Launch Date",
+                inputType: "input",
+            },
+            {
+                id: "mainnetLaunch.evidenceLink",
+                name: "Evidence Link",
+                inputType: "input",
+            },
+        ],
+    },
+]
+
 /* Hacky solution to z-index issue
 For some reason that I couldn't work out, the selectors didn't have the correct z-indexes when they were rendered in the modal.
 So, I had to manually calculate the z-indexes for each option, and pass them down to the EditorOption component.
@@ -52,9 +115,25 @@ zIndex={calcZIndex("status") - 2}
 
 That's why the calcZIndex multiplies the index of the option by 100 so it is very unlikely to clash with other z-indexes
 */
-const allOptions = ["optionSelector", "name", "stakingType", "status", "statusTest", "mainnetLaunch.date", "mainnetLaunch.evidenceLink"]
-const calcZIndex = (option) => {
-    return (allOptions.length - allOptions.indexOf(option)) * 100
+const calcZIndex = (fieldId) => {
+    // Find the option that contains the field
+    const option = allOptions.find((option) => option.fields.some((field) => field.id === fieldId))
+
+    // If the option is not found, return a default value
+    if (!option) return 0
+
+    // Calculate the base z-index based on the option's position
+    const optionIndex = allOptions.indexOf(option)
+    const baseZIndex = (allOptions.length - optionIndex) * 100
+
+    // If the option only has one field, return the base z-index
+    if (option.fields.length === 1) return baseZIndex
+
+    // If the option has multiple fields, find the field's position
+    const fieldIndex = option.fields.findIndex((field) => field.id === fieldId)
+
+    // Subtract the field's position from the base z-index
+    return baseZIndex - fieldIndex
 }
 
 export default function EditorModal({ isOpen, onClose, provider }) {
@@ -103,7 +182,7 @@ export default function EditorModal({ isOpen, onClose, provider }) {
                             <Text isTruncated>{provider.name}</Text>
                         </Flex>
                         <OptionSelector
-                            zIndex={calcZIndex("optionSelector")}
+                            zIndex={999999}
                             provider={provider}
                             currentSelection={currentSelection}
                             setCurrentSelection={setCurrentSelection}
@@ -112,97 +191,33 @@ export default function EditorModal({ isOpen, onClose, provider }) {
                     </Flex>
                 </ModalHeader>
                 <ModalBody overflowY={"scroll"} pt={0} h={"fit-content"}>
-                    {(currentSelection == "name" || currentSelection == "allOptions") && (
-                        <Flex className={"editorOptionContainer"} direction={"column"} gap={3}>
-                            <EditorOptionHeader id={"name"} name={`Update Name`} updatedValues={updatedValues} setUpdatedValues={setUpdatedValues} />
-                            <EditorOption
-                                zIndex={calcZIndex("name")}
-                                id="name"
-                                name="Name"
-                                inputType="input"
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                                provider={provider}
-                            />
-                        </Flex>
-                    )}
-                    {(currentSelection == "stakingType" || currentSelection == "allOptions") && (
-                        <Flex className={"editorOptionContainer"} direction={"column"} gap={3}>
-                            <EditorOptionHeader
-                                id={"stakingType"}
-                                name={`Update Staking Type`}
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                            />
-                            <EditorOption
-                                zIndex={calcZIndex("stakingType")}
-                                id="stakingType"
-                                name="Staking Type"
-                                inputType="select"
-                                options={dataProps
-                                    .flatMap((dataProp) => dataProp)
-                                    .find((prop) => prop?.id === "stakingType")
-                                    ?.options.map((option) => ({
-                                        value: option.value,
-                                        name: providerProperties.find((prop) => prop.value === option.value)?.name,
-                                    }))}
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                                provider={provider}
-                            />
-                        </Flex>
-                    )}
-                    {(currentSelection == "status" || currentSelection == "allOptions") && (
-                        <Flex className={"editorOptionContainer"} direction={"column"} gap={3}>
-                            <EditorOptionHeader
-                                id={"status"}
-                                name={`Update Status`}
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                            />
-                            <EditorOption
-                                zIndex={calcZIndex("status")}
-                                id="status"
-                                name="Status"
-                                inputType="select"
-                                options={[
-                                    { value: "active", name: "Active" },
-                                    { value: "dev", name: "In development" },
-                                ]}
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                                provider={provider}
-                            />
-                        </Flex>
-                    )}
-                    {(currentSelection == "mainnetLaunch" || currentSelection == "allOptions") && (
-                        <Flex className={"editorOptionContainer"} direction={"column"} gap={3}>
-                            <EditorOptionHeader
-                                id={"mainnetLaunch"}
-                                name={`Update Mainnet Launch Date`}
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                            />
-                            <EditorOption
-                                zIndex={calcZIndex("mainnetLaunch.date")}
-                                id="mainnetLaunch.date"
-                                name="Mainnet Launch Date"
-                                inputType="input"
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                                provider={provider}
-                            />
-                            <EditorOption
-                                zIndex={calcZIndex("mainnetLaunch.evidenceLink")}
-                                id="mainnetLaunch.evidenceLink"
-                                name="Evidence Link"
-                                inputType="input"
-                                updatedValues={updatedValues}
-                                setUpdatedValues={setUpdatedValues}
-                                provider={provider}
-                            />
-                        </Flex>
-                    )}
+                    {allOptions.map((option, index) => {
+                        return (
+                            (currentSelection == option.id || currentSelection == "allOptions") && (
+                                <Flex className={"editorOptionContainer"} direction={"column"} gap={3} key={index}>
+                                    <EditorOptionHeader
+                                        id={option.id}
+                                        name={option.headerName}
+                                        updatedValues={updatedValues}
+                                        setUpdatedValues={setUpdatedValues}
+                                    />
+                                    {option.fields.map((field, fieldIndex) => (
+                                        <EditorOption
+                                            zIndex={calcZIndex(field.id)}
+                                            id={field.id}
+                                            name={field.name}
+                                            inputType={field.inputType}
+                                            options={field.options}
+                                            updatedValues={updatedValues}
+                                            setUpdatedValues={setUpdatedValues}
+                                            provider={provider}
+                                            key={fieldIndex}
+                                        />
+                                    ))}
+                                </Flex>
+                            )
+                        )
+                    })}
                 </ModalBody>
                 <ModalFooter>
                     <EditorFooter onClose={closeEditor} provider={provider} updatedValues={updatedValues} setUpdatedValues={setUpdatedValues} />
